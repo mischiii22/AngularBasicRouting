ce1751606c241a8e0989feceb4075521b8d85a90
mischiii22 <cristinacoronel2200@gmail.com>
2024-09-21 13:38:07 +0800

    first Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import {RouterModule, Routes} from "@angular/router";
+import {AppComponent} from "./app.component";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {config} from "rxjs";
+import {HomepageComponent} from "./components/homepage/homepage.component";
+import {LoginComponent} from "./components/login/login.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {SignupComponent} from "./components/signup/signup.component";
+
+
+const routes: Routes = [
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'landingpage', component: LandingPageComponent},
+]
+
+
+@NgModule({
+
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {enableTracing: true}),
+
+  ],
+
+  declarations: [
+AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    LandingPageComponent,
+    SignupComponent,
+  ],
+
+  providers: [
+
+  ],
+
+  bootstrap: [
+    AppComponent
+  ],
+
+  })
+
+export class AppModule {}
