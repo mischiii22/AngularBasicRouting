12827ad27bd48ba12612448669c1715fa046235a
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:02:27 +0800

    #10currencyconverter

---
+++ src/app/components/currencyconverter/currencyconverter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currencyconverter',
+  standalone: true,
+  imports: [],
+  templateUrl: './currencyconverter.component.html',
+  styleUrl: './currencyconverter.component.css'
+})
+export class CurrencyconverterComponent {
+
+}
