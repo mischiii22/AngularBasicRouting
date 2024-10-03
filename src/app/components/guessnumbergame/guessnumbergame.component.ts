ed669d7c3338bbcc2c8f5d3c47427a2442d25f4a
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:12:03 +0800

    #25guessnumbergame

---
+++ src/app/components/guessnumbergame/guessnumbergame.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guessnumbergame',
+  standalone: true,
+  imports: [],
+  templateUrl: './guessnumbergame.component.html',
+  styleUrl: './guessnumbergame.component.css'
+})
+export class GuessnumbergameComponent {
+
+}
