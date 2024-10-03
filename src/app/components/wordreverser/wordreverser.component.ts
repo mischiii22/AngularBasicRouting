331fbd80c55971b25837cd0636e70db28cd7d5e1
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:03:54 +0800

    #12wordreverser

---
+++ src/app/components/wordreverser/wordreverser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wordreverser',
+  standalone: true,
+  imports: [],
+  templateUrl: './wordreverser.component.html',
+  styleUrl: './wordreverser.component.css'
+})
+export class WordreverserComponent {
+
+}
