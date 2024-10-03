93bb084fd3e89ce06585410d4abe96729c0a505c
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:23:39 +0800

    #35fibonaccigenerator

---
+++ src/app/components/fibonaccigenerator/fibonaccigenerator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fibonaccigenerator',
+  standalone: true,
+  imports: [],
+  templateUrl: './fibonaccigenerator.component.html',
+  styleUrl: './fibonaccigenerator.component.css'
+})
+export class FibonaccigeneratorComponent {
+
+}
