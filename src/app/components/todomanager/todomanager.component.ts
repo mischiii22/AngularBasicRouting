3fb86a2d0fa7ee8e535b044eadfd1439ae2213c3
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:11:30 +0800

    #24todomanager

---
+++ src/app/components/todomanager/todomanager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todomanager',
+  standalone: true,
+  imports: [],
+  templateUrl: './todomanager.component.html',
+  styleUrl: './todomanager.component.css'
+})
+export class TodomanagerComponent {
+
+}
