8d83695b732a5fe5c8f985258907c42cf9dec8f9
Aldrindar <andrestablazon@gmail.com>
2024-10-03 13:40:51 +0800

    #3displayname

---
+++ src/app/components/displayname/displayname.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayname',
+  standalone: true,
+  imports: [],
+  templateUrl: './displayname.component.html',
+  styleUrl: './displayname.component.css'
+})
+export class DisplaynameComponent {
+
+}
