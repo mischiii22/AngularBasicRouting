aadb086575396cb31cce9ca6041c408756a32745
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:00:23 +0800

    #7usergreeting

---
+++ src/app/components/usergreeting/usergreeting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usergreeting',
+  standalone: true,
+  imports: [],
+  templateUrl: './usergreeting.component.html',
+  styleUrl: './usergreeting.component.css'
+})
+export class UsergreetingComponent {
+
+}
