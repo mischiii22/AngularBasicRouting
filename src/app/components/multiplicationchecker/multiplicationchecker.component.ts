48bcfc1ab8836b77db6b253d11e4ea2d684a3553
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:13:36 +0800

    #28multiplicationchecker

---
+++ src/app/components/multiplicationchecker/multiplicationchecker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplicationchecker',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiplicationchecker.component.html',
+  styleUrl: './multiplicationchecker.component.css'
+})
+export class MultiplicationcheckerComponent {
+
+}
