b09b078478afe0008da15027119e9afea29d19fb
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:08:30 +0800

    #20palindromechecker

---
+++ src/app/components/palindromechecker/palindromechecker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palindromechecker',
+  standalone: true,
+  imports: [],
+  templateUrl: './palindromechecker.component.html',
+  styleUrl: './palindromechecker.component.css'
+})
+export class PalindromecheckerComponent {
+
+}
