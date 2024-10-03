0c167093d479e415367a8a1740248de2231e9d89
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:07:47 +0800

    #19charactercounter

---
+++ src/app/components/charactercounter/charactercounter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-charactercounter',
+  standalone: true,
+  imports: [],
+  templateUrl: './charactercounter.component.html',
+  styleUrl: './charactercounter.component.css'
+})
+export class CharactercounterComponent {
+
+}
