597befe381aec316c73ece704b66623a3a3038b9
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:05:31 +0800

    #15multiplicationtable

---
+++ src/app/components/multiplicationtable/multiplicationtable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplicationtable',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiplicationtable.component.html',
+  styleUrl: './multiplicationtable.component.css'
+})
+export class MultiplicationtableComponent {
+
+}
