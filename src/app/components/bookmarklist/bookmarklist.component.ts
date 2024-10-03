6e77fe6900ecc9de76e7ae4a9f5f2898ec89a9d5
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:07:11 +0800

    #18bookmarklist

---
+++ src/app/components/bookmarklist/bookmarklist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmarklist',
+  standalone: true,
+  imports: [],
+  templateUrl: './bookmarklist.component.html',
+  styleUrl: './bookmarklist.component.css'
+})
+export class BookmarklistComponent {
+
+}
