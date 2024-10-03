17ebf044a46b6e48a8ed3a8f9b98be2a3d41682f
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:13:06 +0800

    #27randomnumbergenerator

---
+++ src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomnumbergenerator',
+  standalone: true,
+  imports: [],
+  templateUrl: './randomnumbergenerator.component.html',
+  styleUrl: './randomnumbergenerator.component.css'
+})
+export class RandomnumbergeneratorComponent {
+
+}
