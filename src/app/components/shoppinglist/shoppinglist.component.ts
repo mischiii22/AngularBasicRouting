7a25934648ad2f714959b092c34e44a2cadab96b
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:10:10 +0800

    #22shoppinglist

---
+++ src/app/components/shoppinglist/shoppinglist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppinglist',
+  standalone: true,
+  imports: [],
+  templateUrl: './shoppinglist.component.html',
+  styleUrl: './shoppinglist.component.css'
+})
+export class ShoppinglistComponent {
+
+}
