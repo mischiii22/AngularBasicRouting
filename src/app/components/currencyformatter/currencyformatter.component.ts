e39b0eb99fcdff28a9d54aea878525297b673a4d
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:24:53 +0800

    #37currencyformatter

---
+++ src/app/components/currencyformatter/currencyformatter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currencyformatter',
+  standalone: true,
+  imports: [],
+  templateUrl: './currencyformatter.component.html',
+  styleUrl: './currencyformatter.component.css'
+})
+export class CurrencyformatterComponent {
+
+}
