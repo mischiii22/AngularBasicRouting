cf95a06b55b36e87e15bab97ac548bb22602a23c
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:14:59 +0800

    #30wordshuffler

---
+++ src/app/components/wordshuffler/wordshuffler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wordshuffler',
+  standalone: true,
+  imports: [],
+  templateUrl: './wordshuffler.component.html',
+  styleUrl: './wordshuffler.component.css'
+})
+export class WordshufflerComponent {
+
+}
