24d5f689e18ef1da8a810c0a6b7ba8689bcf48a5
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:24:26 +0800

    #36oddsumcalculator

---
+++ src/app/components/oddsumcalculator/oddsumcalculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oddsumcalculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './oddsumcalculator.component.html',
+  styleUrl: './oddsumcalculator.component.css'
+})
+export class OddsumcalculatorComponent {
+
+}
