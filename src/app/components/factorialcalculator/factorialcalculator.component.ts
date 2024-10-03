8a1652ea4402e196a90f277a254de45fb7df4441
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:10:54 +0800

    #23factorialcalculator

---
+++ src/app/components/factorialcalculator/factorialcalculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factorialcalculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './factorialcalculator.component.html',
+  styleUrl: './factorialcalculator.component.css'
+})
+export class FactorialcalculatorComponent {
+
+}
