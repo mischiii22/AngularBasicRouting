8ae05beba157bc4ba47f7287dd2dfc0d524dd849
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:26:04 +0800

    #38randomquotedisplay

---
+++ src/app/components/randomquotedisplay/randomquotedisplay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomquotedisplay',
+  standalone: true,
+  imports: [],
+  templateUrl: './randomquotedisplay.component.html',
+  styleUrl: './randomquotedisplay.component.css'
+})
+export class RandomquotedisplayComponent {
+
+}
