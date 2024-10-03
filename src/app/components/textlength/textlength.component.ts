32a48b9e489166294095eb198404f769df4b3f75
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:01:45 +0800

    #9textlength

---
+++ src/app/components/textlength/textlength.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textlength',
+  standalone: true,
+  imports: [],
+  templateUrl: './textlength.component.html',
+  styleUrl: './textlength.component.css'
+})
+export class TextlengthComponent {
+
+}
