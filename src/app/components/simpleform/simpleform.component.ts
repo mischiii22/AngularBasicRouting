9c2d61ef05f0f11becb3ebf8ac4610693871e232
mischii <cristinacoronel2200@gmail.com>
2024-10-03 13:58:20 +0800

    #5simpleform

---
+++ src/app/components/simpleform/simpleform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simpleform',
+  standalone: true,
+  imports: [],
+  templateUrl: './simpleform.component.html',
+  styleUrl: './simpleform.component.css'
+})
+export class SimpleformComponent {
+
+}
