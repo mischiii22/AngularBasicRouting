720103386267d0663d9a5b6b6e18b66f2aa79b95
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:12:31 +0800

    #26wordcounter

---
+++ src/app/components/wordcounter/wordcounter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wordcounter',
+  standalone: true,
+  imports: [],
+  templateUrl: './wordcounter.component.html',
+  styleUrl: './wordcounter.component.css'
+})
+export class WordcounterComponent {
+
+}
