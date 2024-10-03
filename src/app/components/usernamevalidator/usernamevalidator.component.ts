337d12e0a786ea4384ca9ecb64a297eb5fce8db5
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:15:53 +0800

    #32usernamevalidator

---
+++ src/app/components/usernamevalidator/usernamevalidator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usernamevalidator',
+  standalone: true,
+  imports: [],
+  templateUrl: './usernamevalidator.component.html',
+  styleUrl: './usernamevalidator.component.css'
+})
+export class UsernamevalidatorComponent {
+
+}
