727974ed495971de516390e5b7df1a2a8e19835d
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:09:29 +0800

    #21temperatureconverter

---
+++ src/app/components/temperatureconverter/temperatureconverter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temperatureconverter',
+  standalone: true,
+  imports: [],
+  templateUrl: './temperatureconverter.component.html',
+  styleUrl: './temperatureconverter.component.css'
+})
+export class TemperatureconverterComponent {
+
+}
