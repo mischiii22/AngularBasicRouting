9f30c32279f6e67a73aad6b44c734191c234b0dc
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:33:17 +0800

    #46vowelcounter

---
+++ src/app/components/vowelcounter/vowelcounter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vowelcounter',
+  standalone: true,
+  imports: [],
+  templateUrl: './vowelcounter.component.html',
+  styleUrl: './vowelcounter.component.css'
+})
+export class VowelcounterComponent {
+
+}
