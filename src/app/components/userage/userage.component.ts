20e1157148282f10ba38c11360fef9dabdc88781
mischii <cristinacoronel2200@gmail.com>
2024-10-03 13:59:38 +0800

    #6userage

---
+++ src/app/components/userage/userage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userage',
+  standalone: true,
+  imports: [],
+  templateUrl: './userage.component.html',
+  styleUrl: './userage.component.css'
+})
+export class UserageComponent {
+
+}
