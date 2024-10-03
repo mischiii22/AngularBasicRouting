0c56532e80287c78721174a81e07e1f235910fa9
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:15:29 +0800

    #31bmisolver

---
+++ src/app/components/bmisolver/bmisolver.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bmisolver',
+  standalone: true,
+  imports: [],
+  templateUrl: './bmisolver.component.html',
+  styleUrl: './bmisolver.component.css'
+})
+export class BmisolverComponent {
+
+}
