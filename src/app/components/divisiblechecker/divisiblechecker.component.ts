7cde57e2213640c2da9c7c041ea3cbaf8d5a07c3
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:27:24 +0800

    #40divisiblechecker

---
+++ src/app/components/divisiblechecker/divisiblechecker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-divisiblechecker',
+  standalone: true,
+  imports: [],
+  templateUrl: './divisiblechecker.component.html',
+  styleUrl: './divisiblechecker.component.css'
+})
+export class DivisiblecheckerComponent {
+
+}
