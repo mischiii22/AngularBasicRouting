0974cdbd8ec73d85e3c83eb29fdda09668db19fb
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:03:00 +0800

    #11evenoddchecker

---
+++ src/app/components/evenoddchecker/evenoddchecker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evenoddchecker',
+  standalone: true,
+  imports: [],
+  templateUrl: './evenoddchecker.component.html',
+  styleUrl: './evenoddchecker.component.css'
+})
+export class EvenoddcheckerComponent {
+
+}
