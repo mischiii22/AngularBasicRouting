6e2c6dc5a5b2194092d0e48cfd1a48412fd2bc37
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:06:37 +0800

    #17fahrenheittocelsius

---
+++ src/app/components/fahrenheittocelsius/fahrenheittocelsius.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fahrenheittocelsius',
+  standalone: true,
+  imports: [],
+  templateUrl: './fahrenheittocelsius.component.html',
+  styleUrl: './fahrenheittocelsius.component.css'
+})
+export class FahrenheittocelsiusComponent {
+
+}
