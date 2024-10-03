e2c014113c5a5aafb01311603e320d8477300a93
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:05:00 +0800

    #14showusername

---
+++ src/app/components/showusername/showusername.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showusername',
+  standalone: true,
+  imports: [],
+  templateUrl: './showusername.component.html',
+  styleUrl: './showusername.component.css'
+})
+export class ShowusernameComponent {
+
+}
