ecee17a2bcace43e7ea7fc9e154e9777fd8cca6c
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:14:24 +0800

    #29uppercaseconverter

---
+++ src/app/components/uppercaseconverter/uppercaseconverter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uppercaseconverter',
+  standalone: true,
+  imports: [],
+  templateUrl: './uppercaseconverter.component.html',
+  styleUrl: './uppercaseconverter.component.css'
+})
+export class UppercaseconverterComponent {
+
+}
