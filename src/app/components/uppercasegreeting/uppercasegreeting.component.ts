2c82f0fe7a6503570021f247df4473e7ae85b019
mischii <cristinacoronel2200@gmail.com>
2024-10-03 15:26:35 +0800

    #39uppercasegreeting

---
+++ src/app/components/uppercasegreeting/uppercasegreeting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uppercasegreeting',
+  standalone: true,
+  imports: [],
+  templateUrl: './uppercasegreeting.component.html',
+  styleUrl: './uppercasegreeting.component.css'
+})
+export class UppercasegreetingComponent {
+
+}
