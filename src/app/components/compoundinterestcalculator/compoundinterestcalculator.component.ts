d074dd82ea2e620452a4bc62be4c180090fced27
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:16:59 +0800

    #34compoundinterestcalculator

---
+++ src/app/components/compoundinterestcalculator/compoundinterestcalculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compoundinterestcalculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './compoundinterestcalculator.component.html',
+  styleUrl: './compoundinterestcalculator.component.css'
+})
+export class CompoundinterestcalculatorComponent {
+
+}
