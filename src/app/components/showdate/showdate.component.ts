764ddcb551177678127bacbde591ebe62317ad77
mischii <cristinacoronel2200@gmail.com>
2024-10-03 14:04:30 +0800

    #13showdate

---
+++ src/app/components/showdate/showdate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showdate',
+  standalone: true,
+  imports: [],
+  templateUrl: './showdate.component.html',
+  styleUrl: './showdate.component.css'
+})
+export class ShowdateComponent {
+
+}
